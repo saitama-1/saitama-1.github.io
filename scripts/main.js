f9fe7326089d7ec989844e9a3c48e1191ecab631
Nguyen-Vinh-Quang <87206438+saitama-1@users.noreply.github.com>
2021-08-30 02:15:43 +0700

    init website

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+myButton.onclick = function () {
+    setUserName();
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
